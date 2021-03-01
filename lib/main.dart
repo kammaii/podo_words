ea9078373a0b2e3af28284e776f7fe9e22b4f681
kammaii <kammaii@naver.com>
2021-03-01 11:09:01 +0700

    start podo_words project

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'podo_words',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('podo_words'),
+
+      ),
+      body: Center(
+        child: Column(),
+      ),
+    );
+  }
+}
+
