5f5267c4883dda5904e7f112b81c8e7aae6f2566
kammaii <kammaii@naver.com>
2021-06-08 21:49:09 +0700

    made assemble hangul but not solved batchim problem

--- lib/learning_words_quiz3.dart
+++ lib/learning_words_quiz3.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:podo_words/divider_text.dart';
+import 'package:podo_words/play_audio.dart';
 import 'package:podo_words/word.dart';
 import 'mix_index.dart';
 import 'my_colors.dart';
@@ -17,45 +18,98 @@ class LearningWordsQuiz3 extends StatefulWidget {
 }
 
 class _LearningWordsQuiz3State extends State<LearningWordsQuiz3> {
-  int wordIndex = 0;
+  int quizIndex = 0;
   String front = "";
   String back = "";
-  List<String> hangul = [];
+  List<String> jamo = [];
+  List<int> jamoDecimal = [];
   List<int> mixedIndex = [];
+  int answerCount = 0;
+  List<int> clickedIndex = [];
 
-
-  List<String> cho = ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ" ,"ㅆ", "ㅇ" ,"ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ" ,"ㅍ", "ㅎ"];
-  List<String> jung =["ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"];
-  List<String> jong = ["", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
+  List<String> choList = ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ" ,"ㅆ", "ㅇ" ,"ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ" ,"ㅍ", "ㅎ"];
+  List<String> jungList =["ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"];
+  List<String> jongList = ["", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
 
 
   List<String> decomposeHangul(String front) {
     List<String> frontSplit = front.split('');
     List<String> decomposed = [];
+    jamoDecimal = [];
     for(String str in frontSplit) {
       var uniCode = toRune(str);
-      int cho = ((((uniCode - 0xAC00) / 28) / 21) + 0x1100).toInt();
-      int jung = ((((uniCode - 0xAC00) / 28) % 21) + 0x1161).toInt();
-      int jong = ((((uniCode - 0xAC00) % 28)) + 0x11A8 - 1).toInt();
-      decomposed.add(String.fromCharCode(cho));
-      decomposed.add(String.fromCharCode(jung));
-      if(jong != 4519) {
-        decomposed.add(String.fromCharCode(jong));
+
+      int cho = ((uniCode - 44032) / 28) ~/ 21;
+      int jung = (((uniCode - 44032) / 28) % 21).toInt();
+      int jong = ((uniCode - 44032) % 28).toInt();
+
+      decomposed.add(choList[cho]);
+      decomposed.add(jungList[jung]);
+      if(jong != 0) {
+        decomposed.add(jongList[jong]);
       }
+
+      jamoDecimal.add(cho);
+      jamoDecimal.add(jung);
+      jamoDecimal.add(jong);
     }
     return decomposed;
+  }
+
+  //todo: 입력한 자모음이 순서에 맞으면 글자로 만들기
+
+  String answer = '';
+  int answerCho = 0;
+  int answerJung = 0;
+  int answerJong = 0;
+
+  void setAnswer(int answeredDecimal) { // mixedIndex
+    if(answerCount%3 == 0) { // 초성
+      answer = answer + jamo[answerCount];
+      answerCho = answeredDecimal;
+      answerCount++;
+
+    } else if (answerCount%3 == 1) { // 중성
+      answer = answer.substring(0, answer.length - 1);
+      answer = answer + assembleHangul(cho: answerCho, jung: answeredDecimal);
+      answerJung = answeredDecimal;
+      answerCount++;
+
+    } else {  // 종성
+      answer = answer.substring(0, answer.length - 1);
+      answer = answer + assembleHangul(cho: answerCho, jung: answerJung, jong: answeredDecimal);
+      answerCho = 0;
+      answerJung = 0;
+      answerJong = 0;
+      answerCount++;
+    }
+  }
+
+  String assembleHangul({required int cho, int jung = 0, int jong = 0}) {
+    int decimal = (cho * 588 + jung * 28 + jong) + 44032;
+    return String.fromCharCode(decimal);
+  }
+
+  void setQuiz() {
+    front = widget.words[quizIndex].front;
+    back = widget.words[quizIndex].back;
+    jamo = decomposeHangul(front);
+    mixedIndex = MixIndex().getMixedIndex(jamo.length);
+    answer = '';
+    clickedIndex = [];
+    print('정답 : $front');
+    print('자모 : $jamo');
+    print('자모십진수 : $jamoDecimal');
+    print('랜덤인덱스 : $mixedIndex');
   }
 
 
   @override
   Widget build(BuildContext context) {
 
-    front = widget.words[wordIndex].front;
-    back = widget.words[wordIndex].back;
-
-    hangul = decomposeHangul(front);
-    mixedIndex = MixIndex().getMixedIndex(hangul.length);
-
+    if(answerCount == 0) {
+      setQuiz();
+    }
 
     return Scaffold(
       body: SafeArea(
@@ -86,13 +140,13 @@ class _LearningWordsQuiz3State extends State<LearningWordsQuiz3> {
                   borderRadius: BorderRadius.circular(10.0),
                   child: Padding(
                     padding: const EdgeInsets.all(15.0),
-                    child: Center(child: Text(front, textScaleFactor: 1.5)),
+                    child: Center(child: Text(answer, textScaleFactor: 1.5)),
                   ),
                 ),
                 SizedBox(height: 20.0),
                 GridView.builder(
                     shrinkWrap: true,
-                    itemCount: hangul.length,
+                    itemCount: jamo.length,
                     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount (
                       crossAxisCount: 4,
                       childAspectRatio: 1,
@@ -100,21 +154,54 @@ class _LearningWordsQuiz3State extends State<LearningWordsQuiz3> {
                       mainAxisSpacing: 10.0,
                     ),
                     itemBuilder: (context, index) {
+                      Color bgBtn;
+                      TextDecoration textDecoration;
+                      if(clickedIndex.contains(index)) {
+                        bgBtn = MyColors().purpleLight;
+                        textDecoration = TextDecoration.lineThrough;
+                      } else {
+                        bgBtn = Colors.white;
+                        textDecoration = TextDecoration.none;
+                      }
+
                       return Padding(
                         padding: const EdgeInsets.all(1.0),
                         child: InkWell(
                           onTap: (){
-                            setState(() {
+                            //정답 체크
+                            int answeredDecimal = jamoDecimal[mixedIndex[index]];
+                            if(answeredDecimal == jamoDecimal[answerCount]) { // 정답
+                              setState(() {
+                                PlayAudio().playCorrect();
+                                clickedIndex.add(index);
+                                setAnswer(answeredDecimal);
 
-                            });
+                                if(answerCount >= jamo.length) {  // 다음 퀴즈로 넘어가기
+                                  answerCount = 0;
+                                  quizIndex++;
+
+                                  if(quizIndex >= widget.words.length) {  // 모든 퀴즈 완료
+                                    Navigator.pop(context);
+                                  }
+                                }
+                              });
+                              
+                            } else {
+                              PlayAudio().playWrong();
+                            }
                           },
                           child: Container(
                               decoration: BoxDecoration(
-                                  color: Colors.white,
+                                  color: bgBtn,
                                   borderRadius: BorderRadius.circular(20.0),
                               ),
                               child: Center(
-                                child: Text(hangul[mixedIndex[index]], textScaleFactor: 1.5),
+                                child: Text(jamo[mixedIndex[index]],
+                                  textScaleFactor: 1.5,
+                                  style: TextStyle(
+                                    decoration: textDecoration
+                                  ),
+                                ),
                               )
                           ),
                         ),
