4e3c4f6bacc710c689fcc4b4d583377e9189f1a6
kammaii <kammaii@naver.com>
2021-06-17 20:37:51 +0700

    start inapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:podo_words/main_learning.dart';
+import 'package:in_app_purchase/in_app_purchase.dart';
+import 'package:in_app_purchase_android/in_app_purchase_android.dart';
+import 'package:provider/provider.dart';
 
-
-void main() => runApp(MyApp());
+void main() {
+  InAppPurchaseAndroidPlatformAddition.enablePendingPurchases();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    ChangeNotifierProvider<DashPurchases>(
+      create: (context) => DashPurchases(
+        context.read<DashCounter>(),
+      ),
+      lazy: false,
+    ),
+
     return MaterialApp(
       title: 'podo_words',
       theme: ThemeData(
