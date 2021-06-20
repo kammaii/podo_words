b5e0586263f12e03c84d65cadf2679993b859b69
kammaii <kammaii@naver.com>
2021-06-20 22:24:48 +0700

    fixed new main

--- lib/logo.dart
+++ lib/logo.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'data_storage.dart';
 import 'main_frame.dart';
-import 'my_colors.dart';
 
 class Logo extends StatelessWidget {
   const Logo({Key? key}) : super(key: key);
@@ -9,14 +9,15 @@ class Logo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     DataStorage dataStorage = DataStorage();
-    bool isLoading = true;
 
     return FutureBuilder(
       future: dataStorage.init(),
       builder: (BuildContext context, AsyncSnapshot<List<Object>> snapshot) {
         if(snapshot.hasData) {
           print('데이타 있음 : $snapshot');
-          isLoading = false;
+          SchedulerBinding.instance!.addPostFrameCallback((timeStamp) {
+            Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => MainFrame()));
+          });
         }
         return Scaffold(
             body: Stack(
@@ -25,45 +26,17 @@ class Logo extends StatelessWidget {
                 Center(child: Image.asset('assets/images/podo.png')),
                 Positioned(
                   bottom: 100.0,
-                  child: Column(
+                  child: Row(
                     children: [
-                      Visibility(
-                        visible: isLoading,
-                        child: Row(
-                          children: [
-                            SizedBox(
-                              child: CircularProgressIndicator(
-                                strokeWidth: 1.0,
-                              ),
-                              height: 20.0,
-                              width: 20.0,
-                            ),
-                            SizedBox(width: 20.0),
-                            Text('Loading...')
-                          ],
+                      SizedBox(
+                        child: CircularProgressIndicator(
+                          strokeWidth: 1.0,
                         ),
+                        height: 20.0,
+                        width: 20.0,
                       ),
-                      Visibility(
-                        visible: !isLoading,
-                        child: InkWell(
-                          onTap: (){
-                            Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => MainFrame()));
-                          },
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: MyColors().purple,
-                              borderRadius: BorderRadius.circular(30.0),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                              child: Center(
-                                  child: Text('Start', textScaleFactor: 1.5, style: TextStyle(color: Colors.white),
-                                  )
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
+                      SizedBox(width: 20.0),
+                      Text('Loading...')
                     ],
                   ),
                 ),
