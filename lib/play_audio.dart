9031dc5127ca5c3db0d63606d2cae5aae1d1a24c
kammaii <kammaii@naver.com>
2021-11-10 20:40:34 +0700

    fixed effect sound volume (1.1.4)

--- lib/play_audio.dart
+++ lib/play_audio.dart
@@ -16,21 +16,25 @@ class PlayAudio {
 
   void playCorrect() async {
     await player.setAsset('assets/audio/correct.mp3');
+    await player.setVolume(0.1);
     player.play();
   }
 
   void playWrong() async {
     await player.setAsset('assets/audio/wrong.mp3');
+    await player.setVolume(0.1);
     player.play();
   }
 
   void playYay() async {
     await player.setAsset('assets/audio/yay.mp3');
+    await player.setVolume(0.1);
     player.play();
   }
 
   void playWord(String audio) async {
     await player.setAsset('assets/audio/$audio');
+    await player.setVolume(1);
     player.play();
   }
 }
